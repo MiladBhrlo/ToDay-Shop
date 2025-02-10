9ec0a9c4235e03753c7e489b15edb547762f82a2
milad baharlo <85839442+MiladBhrlo@users.noreply.github.com>
2025-02-10 12:15:24 +0330

    feat: add order contracts

---
+++ Orders/src/01-Core/ToDay-Shop.Orders.Core.Contracts/Orders/Commands/IOrderCommandRepository.cs
@@ -0,0 +1,7 @@
+﻿using ToDay_Shop.Orders.Core.Contracts.Common.Data.Commands;
+using ToDay_Shop.Orders.Core.Domain.Orders.Entities;
+
+namespace ToDay_Shop.Orders.Core.Contracts.Orders.Commands;
+public interface IOrderCommandRepository : ICommandRepository<Order>
+{
+}
